d1b5ca1c4ca12906f7feeee2a411c4078cbb071e
Ethan Mayer <ethan.mayer@comcast.net>
2023-06-26 15:55:24 -0500

    Barometer class total refactor, now async with promises

--- DragRacer/DragRacer/DataGathering/Barometer.swift
+++ DragRacer/DragRacer/DataGathering/Barometer.swift
@@ -8,38 +8,61 @@
 
 import Foundation
 import CoreMotion
-import PromiseKit
+import Combine
+import SwiftUILogger
 
 class Barometer {
     
-    //Initialize pressure sensor
+    // Initialize pressure sensor
     let altimeter = CMAltimeter()
     
-    var inchesOfMercury = 0.0
+    var pressure = 0.0
 
-    //Get the current pressure from the barometer
-    func getPressure() -> Promise<Double> {
-        return Promise { seal in
-            if CMAltimeter.isRelativeAltitudeAvailable() {
-                altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main) { (data, error) in
-                    //MARK: TODO: Use for < iOS13?
-                    //Native data is in units of kPa
-                    //let kilopascals = (data?.pressure.floatValue)!
-                    //Convert data to US standard pressure units of inHg
-                    //1 atm = 101.325 kPa | 101,325 Pa = 29.92 inHg
-                    //let inchesOfMercury = kilopascals / 101.325 * 29.92 //inHg
+    // Get the current pressure from the barometer, return as Future
+    func getPressure() -> Future<Double, Error> {
+        return Future<Double, Error> { promise in
+            
+            // Proceed async
+            DispatchQueue.main.async {
+        
+                // Check if altimeter is working
+                if CMAltimeter.isRelativeAltitudeAvailable() {
                     
-                    //If there is no error, proceed
-                    if (error == nil) {
-                        //Get pressure in inHg
-                        self.inchesOfMercury = Measurement(value: (data?.pressure.doubleValue)!, unit: UnitPressure.kilopascals).converted(to: .inchesOfMercury).value //MARK: TODO: convert when displaying, and save original value; display it based on a setting.
-                        print(String(self.inchesOfMercury))
+                    // Check authorization status of altimeter
+                    switch CMAltimeter.authorizationStatus() {
+                        case .notDetermined: // Handle state before user prompt
+                            logger.log(level: .info, message: "Altimiter authorization undetermined") //MARK: TODO: .debug
+                        case .restricted: // Handle system-wide restriction
+                           fatalError("Altimeter authorization restricted")
+                        case .denied: // Handle user denied state
+                            fatalError("Altimeter authorization denied")
+                        case .authorized: // Ready to go!
+                            logger.log(level: .info, message: "Altimeter authorized") //MARK: TODO: .debug
+                        @unknown default:
+                            fatalError("Unknown authorization status")
+                    }
+                    
+                    // Start receiving altitude from altimeter
+                    self.altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main) { (data, error) in
                         
-                        //Stop receiving updates after the pressure has been recorded
-                        self.altimeter.stopRelativeAltitudeUpdates()
-                        seal.fulfill(self.inchesOfMercury)
-                    } else {
-                        seal.reject(error!)
+                        // If there is a non-nil error, return with promise as failure
+                        if let error = error {
+                            promise(.failure(error))
+                            
+                        // If there is no error, proceed
+                        } else {
+                            
+                            // Get pressure in inHg
+                            self.pressure = Measurement(value: (data?.pressure.doubleValue)!, unit: UnitPressure.kilopascals)/*.converted(to: .inchesOfMercury)*/.value
+                            
+                            logger.log(level: .info, message: "Pressure: " + String(self.pressure)) //MARK: TODO: .debug
+                            
+                            // Stop receiving updates after the pressure has been recorded
+                            self.altimeter.stopRelativeAltitudeUpdates()
+                            
+                            // Return value with promise as success
+                            promise(.success(self.pressure))
+                        }
                     }
                 }
             }
