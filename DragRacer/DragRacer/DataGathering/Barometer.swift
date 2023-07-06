79e4e47f7101e63d5365a1275f061dca0d1517c3
Ethan Mayer <ethan.mayer@comcast.net>
2023-07-06 16:10:11 -0500

    Barometer converted to async stream

--- DragRacer/DragRacer/DataGathering/Barometer.swift
+++ DragRacer/DragRacer/DataGathering/Barometer.swift
@@ -15,6 +15,69 @@ extension String: LocalizedError {
     public var errorDescription: String? { return self }
 }
 
+// Async stream implementation for debugging
+class Barometer {
+    
+    // Initialize pressure sensor
+    let altimeter = CMAltimeter()
+    
+    var pressure = 0.0
+
+    // Get the current pressure from the barometer, return as Future
+    func getPressure() -> AsyncThrowingStream<Double, Error> {
+        return AsyncThrowingStream<Double, Error> { continuation in
+            
+            // Proceed async
+            DispatchQueue.main.async {
+        
+                // Check if altimeter is working
+                if CMAltimeter.isRelativeAltitudeAvailable() {
+                    
+                    // Check authorization status of altimeter
+                    switch CMAltimeter.authorizationStatus() {
+                        case .notDetermined: // Handle state before user prompt
+                            logger.log(level: .info, message: "Altimiter authorization undetermined")
+                        case .restricted: // Handle system-wide restriction
+                            continuation.finish(throwing: "Altimeter authorization restricted")
+                        case .denied: // Handle user denied state
+                            continuation.finish(throwing: "Altimeter authorization denied")
+                        case .authorized: // Ready to go!
+                            logger.log(level: .info, message: "Altimeter authorized")
+                        @unknown default:
+                            continuation.finish(throwing: "Unknown authorization status")
+                    }
+                    
+                    // Start receiving altitude from altimeter
+                    self.altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main) { (data, error) in
+                        
+                        // If there is a non-nil error, return with promise as failure
+                        if let error = error {
+                            continuation.finish(throwing: error)
+                            
+                        // If there is no error, proceed
+                        } else {
+                            
+                            // Get pressure in inHg
+                            self.pressure = Measurement(value: (data?.pressure.doubleValue)!, unit: UnitPressure.kilopascals)/*.converted(to: .inchesOfMercury)*/.value
+                            
+//                            logger.log(level: .debug, message: "Pressure: " + String(self.pressure))
+                            continuation.yield(self.pressure)
+                            
+                            // Stop receiving updates after the pressure has been recorded
+                            self.altimeter.stopRelativeAltitudeUpdates()
+                            
+                            // Return value with promise as success
+//                            promise(.success(self.pressure))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+/* Future implementation below
 class Barometer {
     
     // Initialize pressure sensor
@@ -73,3 +136,4 @@ class Barometer {
         }
     }
 }
+*/
--- DragRacer/DragRacer/DataGathering/GPS.swift
+++ DragRacer/DragRacer/DataGathering/GPS.swift
@@ -21,72 +21,72 @@
 //}
 //
 //class GPS: NSObject, CLLocationManagerDelegate {
-//    
+//
 //    //Initialize GPS API
 //    let locationManager = CLLocationManager()
 //    var bestRadiusSoFar: CLLocationAccuracy = CLLocationAccuracy.greatestFiniteMagnitude
-//    
+//
 //    var originalLocation: CLLocation? = nil //Starting location
 //    var originalLocationPlacemark: CLPlacemark? = nil //Starting location's geolocater placemark
 //    var latitude: CLLocationDegrees? = nil //Latitude of the location in degrees
 //    var longitude: CLLocationDegrees? = nil //Longitude of location in degrees
 //    var accurate = false
-//    
+//
 //    let epsilon = 0.1
 //    let Mph60_mps = Measurement(value: 60.0, unit: UnitSpeed.milesPerHour).converted(to: UnitSpeed.metersPerSecond).value // 60mph in m/s
 //    let Ft330_m = Measurement(value: 330.0, unit: UnitLength.feet).converted(to: UnitLength.meters).value // 330ft in m
 //    let MiOneEighth_m = Measurement(value: 0.125, unit: UnitLength.miles).converted(to: UnitLength.meters).value // 1/8th mi in m
 //    let Ft1000_m = Measurement(value: 1000.0, unit: UnitLength.feet).converted(to: UnitLength.meters).value // 1000ft in m
 //    let MiOneFourth_m = Measurement(value: 0.25, unit: UnitLength.miles).converted(to: UnitLength.meters).value // 1/4th mi in m
-//    
+//
 //    var distance: CLLocationDistance = 0
-//    
+//
 //    var race: RaceOptions!
-//    
+//
 //    //Initialize GPS Delegate
 //    weak var delegate: GPSDelegate?
 //    init(delegate: GPSDelegate) {
 //        self.delegate = delegate
 //    }
-//    
+//
 //    //Get GPS location data
 //    func startReceivingLocationChanges() {
 //        let authorizationStatus = CLLocationManager.authorizationStatus()
-//        
+//
 //        //Check if user has authorized the app to use location services
 //        if authorizationStatus != .authorizedWhenInUse && authorizationStatus != .authorizedAlways {
 //            locationManager.requestAlwaysAuthorization()
 //        }
-//        
+//
 //        // Do not start services that aren't available.
 //        if !CLLocationManager.locationServicesEnabled() {
 //            return
 //        }
-//        
+//
 //        // Configure location service
 //        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
 //        locationManager.distanceFilter = kCLDistanceFilterNone  //Notified of all movements
 //        locationManager.delegate = self
-//        
+//
 //        //Start location service
 //        locationManager.startUpdatingLocation()
 //    }
-//    
+//
 //    //Stop getting GPS location data
 //    func stopReceivingLocationChanges() {
 //        locationManager.stopUpdatingLocation()
 //    }
-//    
+//
 //    func setRaceOptions(raceOptions: RaceOptions) {
 //        race = raceOptions
 //    }
-//    
+//
 //    //Handle getting location data
 //    func locationManager(_ locationManager: CLLocationManager, didUpdateLocations: [CLLocation]) {
 //        let lastLocation = didUpdateLocations.last!
 //
 //        print("Current and new best: \(lastLocation.horizontalAccuracy) meter radius")
-//            
+//
 //        if (lastLocation.horizontalAccuracy <= 10) { //Radius of 10 meter radius is acceptable as accurate
 //            if !accurate {
 //                accurate = true
@@ -94,11 +94,11 @@
 //                self.latitude = lastLocation.coordinate.latitude
 //                self.longitude = lastLocation.coordinate.longitude
 //                //print("Best accuracy: \(bestRadiusSoFar) meter radius.")
-//                
+//
 //                //Use CLGeocoder to find the address of the current location
 //                if let lastLocation = self.locationManager.location {
 //                    let geocoder = CLGeocoder()
-//                        
+//
 //                    // Look up the location and pass it to the completion handler
 //                    geocoder.reverseGeocodeLocation(lastLocation,
 //                                completionHandler: { (placemarks, error) in
@@ -116,7 +116,7 @@
 //            } else {
 //                if (locationManager.location!.speed > 0) {
 //                    var finishObjective: Double = 0
-//                    
+//
 //                    //MARK: TODO: Refactor
 //                    //Check to see which objective the user selected as the final race objective
 //                    if (race.contains(.ft330)) {
@@ -128,25 +128,25 @@
 //                    } else if (race.contains(.miOneFourthTime) || race.contains(.miOneFourthSpeed)) {
 //                        finishObjective = MiOneFourth_m
 //                    }
-//                    
+//
 //                    //Notify the race of the current distance from the end of the race
 //                    delegate?.getDistanceTraveledPercentage(percentage: NSNumber(value: locationManager.location!.distance(from: originalLocation!) / finishObjective))
 //                }
-//                
+//
 //                //Check for 60mph
 //                if (locationManager.location!.speed - Mph60_mps < epsilon && locationManager.location!.speed - Mph60_mps > 0 && race.contains(.mph60)) {
 //                    //Notify the race of the time 60mph was reached
 //                    delegate?.get60mphTime(time: clock_gettime_nsec_np(CLOCK_MONOTONIC_RAW))
 //                    race.subtract(.mph60)
 //                }
-//                
+//
 //                //Check for 330ft
 //                if (locationManager.location!.distance(from: originalLocation!) - Ft330_m < epsilon && locationManager.location!.distance(from: originalLocation!) - Ft330_m > 0 && race.contains(.ft330)) {
 //                    //Notify the race of the time 330ft was reached
 //                    delegate?.get330ftTime(time: clock_gettime_nsec_np(CLOCK_MONOTONIC_RAW))
 //                    race.subtract(.ft330)
 //                }
-//                
+//
 //                //Check for 1/8th mile
 //                if (locationManager.location!.distance(from: originalLocation!) - MiOneEighth_m < epsilon && locationManager.location!.distance(from: originalLocation!) - MiOneEighth_m > 0 && (race.contains(.miOneEightTime) || race.contains(.miOneEightSpeed))) {
 //                    //Notify the race of the time 1/8th mile was reached
@@ -159,14 +159,14 @@
 //                        race.subtract(.miOneEightSpeed)
 //                    }
 //                }
-//                
+//
 //                //Check for 1000ft
 //                if (locationManager.location!.distance(from: originalLocation!) - Ft1000_m < epsilon && locationManager.location!.distance(from: originalLocation!) - Ft1000_m > 0 && race.contains(.ft1000)) {
 //                    //Notify the race of the time 1000ft was reached
 //                    delegate?.get1000ftTime(time: clock_gettime_nsec_np(CLOCK_MONOTONIC_RAW))
 //                    race.subtract(.ft1000)
 //                }
-//                
+//
 //                //Check for 1/4th mile
 //                if (locationManager.location!.distance(from: originalLocation!) - MiOneFourth_m < epsilon && locationManager.location!.distance(from: originalLocation!) - MiOneFourth_m > 0 && (race.contains(.miOneFourthTime) || race.contains(.miOneFourthSpeed))) {
 //                    //Notify the race of the time 1/4th mile was reached
@@ -182,7 +182,7 @@
 //            }
 //        }
 //    }
-//    
+//
 //    //Error handling for locationManager
 //    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
 //        if let error = error as? CLError, error.code == .denied {
